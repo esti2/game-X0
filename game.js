1b73f22609211706d985f1a70b8d9ef22870344a
esti2 <estib9509@gmail.com>
2022-05-12 20:59:11 +0300

    finish

--- game.js
+++ game.js
@@ -27,22 +27,20 @@ var player;
 
 var myInterval;
 var TimeToChangeTurn;
-var submit=false;
+//var submit=false;
 
 window.addEventListener("load", setUp);
-//btnSubmit.addEventListener("click",submit);
 
 //setup
 function setUp() {
     leftCells = 0;
     currentPlayer = "X";
-    //xTurn = true;
-    //btnSubmit.addEventListener("click",submit);
 }
 
 //submitPlayers
 function submit() {
-    submit=true;
+    submit = true;
+
     colorP1 = document.getElementById("colorPlayer1").value;
     colorP2 = document.getElementById("colorPlayer2").value;
     name1 = document.getElementById("namePlayer1").value;
@@ -61,6 +59,8 @@ function submit() {
     greeting();
     Timer();
     TimeToChangeTurn = 10000 + 100;
+
+    document.getElementById("turn").innerHTML = ("turn - " + name1 + " X");
 }
 
 //greeting
@@ -85,31 +85,29 @@ function checkWin(x, y) {
     checkRow(x, y);
     checkCol(x, y);
     checkDiagR(x, y);
-    // checkDiagL(x, y);
+    checkDiagL(x, y);
 }
+
+
 
 function checkCol(x, y) {
     var count = 0;
     player = arr[x][y];
-
-    for (v = 0; v < (selRowNum.value % 4) + 1; v++)// num rizot
-    {
+    for (v = 0; v < (selRowNum.value % 4) + 1; v++) {
         count = 0;
         for (m = v; m < 4 + v; m++) {
             if (arr[m][y] == player) {
                 count++;
-                if (x == m)
-                    var check = true;
                 if (count == 1)
                     var temp = m;
-                else if (count == 4 && check) {
+                else if (count == 4 && temp <= x && x <= m) {
                     alert("win col " + player);
                     if (player == "X")
                         winX++;
                     else
                         win0++;
                     for (i = temp; i <= m; i++) {
-                        document.getElementById("myTable").rows[i].cells[y].style.backgroundColor = "000000";
+                        document.getElementById("myTable").rows[i].cells[y].style.backgroundColor = "black";
                         document.getElementById("myTable").rows[i].cells[y].style.color = "#ffffff";
                     }
                 }
@@ -117,46 +115,33 @@ function checkCol(x, y) {
             else
                 m = 4 + v;
         }
-        /* if (count == 4 && check) {
-             alert("win col " + player);
-             if (player == "X")
-                 winX++;
-             else
-                 win0++;
-         }*/
     }
 }
 
 function checkRow(x, y) {
     var count = 0;
     player = arr[x][y];
-
-    for (v = 0; v < (selRowNum.value % 4) + 1; v++)// num rizot
-    {
+    for (v = 0; v < (selRowNum.value % 4) + 1; v++) {
         count = 0;
         for (m = v; m < 4 + v; m++) {
             if (arr[x][m] == player) {
                 count++;
-                if (x == m)
-                    var check = true;
                 if (count == 1)
                     var temp = m;
-                else if (count == 4 && check) {
+                else if (count == 4 && temp <= y && y <= m) {
                     alert("win row " + player);
                     if (player == "X")
                         winX++;
                     else
                         win0++;
                     for (i = temp; i <= m; i++) {
-                        document.getElementById("myTable").rows[x].cells[i].style.backgroundColor = "000000";
+                        document.getElementById("myTable").rows[x].cells[i].style.backgroundColor = "black";
                         document.getElementById("myTable").rows[x].cells[i].style.color = "#ffffff";
                     }
                 }
             }
             else {
                 m = 4 + v;
-                check = false;
-                // v= (selRowNum.value % 4) + 1         
             }
         }
     }
@@ -165,70 +150,124 @@ function checkRow(x, y) {
 function checkDiagR(x, y) {
     var count = 0;
     player = arr[x][y];
-
-    if (x == y) {
-        for (v = 0; v < (selRowNum.value % 4) + 1; v++)// num rizot
-        {
+    if (x >= y) {
+        for (v = 0; v < (selRowNum.value % 4) + 1 - Math.abs(parseInt(x) - parseInt(y)); v++) {
             count = 0;
             for (m = v; m < v + 4; m++) {
-                if (arr[m][m] == player) {
-                    count++;
-                }
-                else
-                    m = 4 + v;
-            }
-            if (count == 4) {
-                alert("win diagR up" + player);
-                if (player == "X")
-                    winX++;
-                else
-                    win0++;
-            }
-        }
-    }
-    else if (x > y) {
-        for (v = 0; v < 4 - (parseInt(x) - parseInt(y)); v++)// num rizot
-        {
-            count = 0;
-            // for (m = x % 4 - (x - y) + 1; m < v + 4; m++) {
-            for (m = v; m < v + 4; m++) {
-                if (m >= x || m < 0 || m + (parseInt(x) - parseInt(y)) >= selRowNum.value - 1)
-                    break;
+                if (m < 0 || m + (parseInt(x) - parseInt(y)) > selRowNum.value - 1)
+                    break;//
                 if (arr[m + (parseInt(x) - parseInt(y))][m] == player) {
                     count++;
+                    if (count == 1)
+                        var temp = m;
+                    if (count == 4 && y >= temp && y <= m) {
+                        alert("win diagR up" + player);
+                        if (player == "X")
+                            winX++;
+                        else
+                            win0++;
+                        for (i = temp; i <= m; i++) {
+                            document.getElementById("myTable").rows[i + (parseInt(x) - parseInt(y))].cells[i].style.backgroundColor = "black";
+                            document.getElementById("myTable").rows[i + (parseInt(x) - parseInt(y))].cells[i].style.color = "#ffffff";
+                        }
+                    }
                 }
                 else
                     m = v + 4;
-            }
-            if (count == 4) {
-                alert("win diagR down" + player);
-                if (player == "X")
-                    winX++;
-                else
-                    win0++;
             }
         }
     }
-
     else {//y>x
-        for (v = 0; v < 4 - (parseInt(y) - parseInt(x)); v++)// num rizot
-        {
+        for (v = 0; v < (selRowNum.value % 4) + 1 - Math.abs(parseInt(x) - parseInt(y)); v++) {
             count = 0;
             for (m = v; m < v + 4; m++) {
-                if (m >= x || m < 0 || m + (parseInt(x) - parseInt(y)) >= selRowNum.value - 1)
-                    break;
+                if (m < 0 || m + (parseInt(y) - parseInt(x)) > selRowNum.value - 1)
+                    break;//
                 if (arr[m][m + (parseInt(y) - parseInt(x))] == player) {
                     count++;
+                    if (count == 1)
+                        var temp = m;
+                    if (count == 4 && x >= temp && x <= m) {
+                        alert("win diagR up" + player);
+                        if (player == "X")
+                            winX++;
+                        else
+                            win0++;
+
+                        for (i = temp; i <= m; i++) {
+                            document.getElementById("myTable").rows[i].cells[i + (parseInt(y) - parseInt(x))].style.backgroundColor = "black";
+                            document.getElementById("myTable").rows[i].cells[i + (parseInt(y) - parseInt(x))].style.color = "#ffffff";
+                        }
+                    }
                 }
                 else
                     m = v + 4;
             }
-            if (count == 4) {
-                alert("win vhvj" + player);
-                if (player == "X")
-                    winX++;
-                else
-                    win0++;
+        }
+    }
+}
+
+function checkDiagL(x, y) {
+    var count = 0;
+    player = arr[x][y];
+    const max = (selRowNum.value - 1) + (selRowNum.value - 4);
+    const min = 3;
+    if (x + y >= min && (x + y) <= max) {
+        if ((x + y) < selRowNum.value)//up
+            for (v = 0; v < ((x + y + 1) % 4) + 1; v++) {
+                count = 0;
+                for (m = v; m < v + 4; m++) {
+                    if (arr[m][(x + y) - m] == player) {
+                        count++;
+                        if (count == 1)
+                            var temp = m;
+                        if (count == 4 && x >= temp && x <= m) {
+                            alert("win diagR up" + player);
+                            if (player == "X")
+                                winX++;
+                            else
+                                win0++;
+                            for (i = temp; i <= m; i++) {
+                                document.getElementById("myTable").rows[i].cells[x + y - i].style.backgroundColor = "black";
+                                document.getElementById("myTable").rows[i].cells[x + y - i].style.color = "#ffffff";
+                            }
+                        }
+                    }
+                    else
+                        m = v + 4;
+                }
+            }
+
+        else {//down
+            var num = 0;
+            if (selRowNum.value % 2 == 0)
+                num = 4 - ((x + y) % 4);
+            else
+                num = 4 - ((x + y + 1) % 4 + 1);
+            for (v = 0; v < num; v++) {
+                count = 0;
+                for (m = v; m < v + 4; m++) {
+                    var first = (x + y) % selRowNum.value + 1 + m;
+                    var last = x + y - first;
+                    if (arr[first][last] == player) {
+                        count++;
+                        if (count == 1)
+                            var temp = m;
+                        if (count == 4 && (y >= temp && y <= m || x >= temp && x <= m)) {
+                            alert("win diagR up" + player);
+                            if (player == "X")
+                                winX++;
+                            else
+                                win0++;
+                            for (i = temp; i <= m; i++) {
+                                document.getElementById("myTable").rows[(x + y) % selRowNum.value + 1 + i].cells[x + y - ((x + y) % selRowNum.value + 1 + i)].style.backgroundColor = "black";
+                                document.getElementById("myTable").rows[(x + y) % selRowNum.value + 1 + i].cells[x + y - ((x + y) % selRowNum.value + 1 + i)].style.color = "#ffffff";
+                            }
+                        }
+                    }
+                    else
+                        m = v + 4;
+                }
             }
         }
     }
@@ -281,10 +320,8 @@ function buildBoard() {
 
 //document.getElementById("myDiv").style.border = "thick solid #0000FF";
 function play() {
-    if(document.getElementById("namePlayer1").value=="" || document.getElementById("namePlayer2").value=="" || submit==false)
-    {
-    alert("You must register your name and click submit");
-
+    if (document.getElementById("namePlayer1").value == "" || document.getElementById("namePlayer2").value == "" || submit == false) {
+        alert("You must register your name and click submit");
     }
     var cell = event.srcElement;
     if (cell.innerHTML != "O" && cell.innerHTML != "X") {
@@ -308,7 +345,6 @@ function play() {
         }
         TimeToChangeTurn = 10000 + 100;
         document.getElementById("myTable").rows[cell.locRow].cells[cell.locCol].style.backgroundColor = color;
-        //cell.style.color = color;
         leftCells++;
         checkWin(cell.locRow, cell.locCol);
         endGame();
@@ -316,10 +352,10 @@ function play() {
 }
 
 function restart() {
-    sizeGame = 0;
     leftCells = 0;
     winX = 0;
     winY = 0;
+    //sizeGame = selRowNum.value * selRowNum.value;
 
     for (var q = 0; q < arr.length; q++) {
         for (var x = 0; x < arr.length; x++) {
@@ -329,27 +365,28 @@ function restart() {
             window.clearInterval(myInterval);
         }
     }
+    Timer();
 }
+
 function Timer() {
-    //document.getElementById("time").style.visibility = "visible";
     myInterval = setInterval(function () {
         TimeToChangeTurn -= 1000;
         var seconds = Math.floor(TimeToChangeTurn / 1000);
         if (currentPlayer == 'X') {
-            document.getElementById("time").innerHTML = currentPlayer + "'s Turn <br>"
-                + seconds + " seconds remaining";
+            document.getElementById("time").innerHTML = seconds;
         }
         else {
-            document.getElementById("time").innerHTML = currentPlayer + "'s Turn <br>"
-                + seconds + " seconds remaining";
+            document.getElementById("time").innerHTML = seconds;
         }
 
         if (seconds <= 3) {
             document.getElementById("time").style.color = "red";
-            document.getElementById("time").style.size = "10";
+            document.getElementById("time").style.fontSize = "x-large";
         }
-        else
-            document.getElementById("time").style.color = "white";
+        else {
+            document.getElementById("time").style.color = "black";
+            document.getElementById("time").style.fontSize = "medium";
+        }
         if (TimeToChangeTurn == 0 || TimeToChangeTurn == 100) {
             TimeToChangeTurn = 10000;
             if (currentPlayer == "X") {
@@ -361,8 +398,8 @@ function Timer() {
                 document.getElementById("player1").style.border = "thick solid #F0A133";
                 document.getElementById("player2").style.border = "none";
                 currentPlayer = "X";
-                document.getElementById("turn").innerHTML = ("turn - " + currentPlayer);
-            }
+
+            } document.getElementById("turn").innerHTML = ("turn - " + currentPlayer);
         }
     }
         , 1000)
